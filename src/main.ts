048fe5eb86ccc6c1ee53f426f1d7c8b5b0a72a71
ShinCoder <shincoder.forwork@gmail.com>
2025-01-20 21:18:57 +0700

    feat: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from 'module/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
 }
 bootstrap();
